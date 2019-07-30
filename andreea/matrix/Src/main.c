f4f6ae5e2c515120a45d5a97e19461a67acdc283
Izota Andreea <andreeaizota@yahoo.com>
2019-07-30 11:35:00 +0300

    matrix 'gogoasa' final version

--- andreea/matrix/Src/main.c
+++ andreea/matrix/Src/main.c
@@ -28,7 +28,44 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+uint8_t disp1ay[38][8]={
+{0x3C,0x42,0x42,0x42,0x42,0x42,0x42,0x3C},//0
+{0x10,0x30,0x50,0x10,0x10,0x10,0x10,0x7c},//1
+{0x7E,0x2,0x2,0x7E,0x40,0x40,0x40,0x7E},//2
+{0x3E,0x2,0x2,0x3E,0x2,0x2,0x3E,0x0},//3
+{0x8,0x18,0x28,0x48,0xFE,0x8,0x8,0x8},//4
+{0x3C,0x20,0x20,0x3C,0x4,0x4,0x3C,0x0},//5
+{0x3C,0x20,0x20,0x3C,0x24,0x24,0x3C,0x0},//6
+{0x3E,0x22,0x4,0x8,0x8,0x8,0x8,0x8},//7
+{0x0,0x3E,0x22,0x22,0x3E,0x22,0x22,0x3E},//8
+{0x3E,0x22,0x22,0x3E,0x2,0x2,0x2,0x3E},//9
+{0x18,0x24,0x42,0x42,0x7E,0x42,0x42,0x42},//A
+{0x3C,0x22,0x22,0x3c,0x22,0x22,0x3C,0x0},//B
+{0x3C,0x40,0x40,0x40,0x40,0x40,0x40,0x3C},//C
+{0x7C,0x22,0x22,0x22,0x22,0x22,0x22,0x7C},//D
+{0x7C,0x40,0x40,0x7C,0x40,0x40,0x40,0x7C},//E
+{0x7C,0x40,0x40,0x7C,0x40,0x40,0x40,0x40},//F
+{0x3C,0x40,0x40,0x40,0x4c,0x44,0x44,0x3C},//G
+{0x44,0x44,0x44,0x7C,0x44,0x44,0x44,0x44},//H
+{0x7C,0x10,0x10,0x10,0x10,0x10,0x10,0x7C},//I
+{0x3C,0x8,0x8,0x8,0x8,0x8,0x48,0x30},//J
+{0x0,0x24,0x28,0x30,0x20,0x30,0x28,0x24},//K
+{0x40,0x40,0x40,0x40,0x40,0x40,0x40,0x7C},//L
+{0x81,0xC3,0xA5,0x99,0x81,0x81,0x81,0x81},//M
+{0x0,0x42,0x62,0x52,0x4A,0x46,0x42,0x0},//N
+{0x3C,0x42,0x42,0x42,0x42,0x42,0x42,0x3C},//O
+{0x3C,0x22,0x22,0x22,0x3C,0x20,0x20,0x20},//P
+{0x1C,0x22,0x22,0x22,0x22,0x26,0x22,0x1D},//Q
+{0x3C,0x22,0x22,0x22,0x3C,0x24,0x22,0x21},//R
+{0x0,0x1E,0x20,0x20,0x3E,0x2,0x2,0x3C},//S
+{0x0,0x3E,0x8,0x8,0x8,0x8,0x8,0x8},//T
+{0x42,0x42,0x42,0x42,0x42,0x42,0x22,0x1C},//U
+{0x42,0x42,0x42,0x42,0x42,0x42,0x24,0x18},//V
+{0x0,0x49,0x49,0x49,0x49,0x2A,0x1C,0x0},//W
+{0x0,0x41,0x22,0x14,0x8,0x14,0x22,0x41},//X
+{0x41,0x22,0x14,0x8,0x8,0x8,0x8,0x8},//Y
+{0x0,0x7F,0x2,0x4,0x8,0x10,0x20,0x7F},//Z
+};
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -43,8 +80,6 @@
 
 /* Private variables ---------------------------------------------------------*/
 
-SPI_HandleTypeDef hspi2;
-
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -52,14 +87,39 @@ SPI_HandleTypeDef hspi2;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_SPI2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void write_byte(uint8_t byte)
+{
+	    for(int i=0;i<8;i++)
+          {
+	    	 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, 0);  // Pull the CLK LOW
+	    	 HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, byte&0x80);// Write one BIT data MSB first
+             byte = byte<<1;  // shift the data to the left
+             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, 1);  // Pull the CLK HIGH
+           }
+}
 
+void write_max (uint8_t address, uint8_t data)
+{
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, 0);   // Pull the CS pin LOW
+    write_byte(address);   //  write address
+    write_byte(data);  //  write data
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, 1);  // pull the CS HIGH
+}
+
+void max_init(void)
+{
+ write_max(0x09, 0x00);       //  no decoding
+ write_max(0x0a, 0x03);       //  brightness intensity
+ write_max(0x0b, 0x07);       //  scan limit = 8 LEDs
+ write_max(0x0c, 0x01);       //  power down =0，normal mode = 1
+ write_max(0x0f, 0x00);       //  no test display
+}
 /* USER CODE END 0 */
 
 /**
@@ -69,7 +129,7 @@ static void MX_SPI2_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	uint8_t data=67;
+
   /* USER CODE END 1 */
   
 
@@ -91,18 +151,51 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_SPI2_Init();
   /* USER CODE BEGIN 2 */
-
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  max_init();
   while (1)
   {
     /* USER CODE END WHILE */
-	  HAL_SPI_Transmit(&hspi2,&data,8,0);
+	  for(int i=1;i<9;i++)
+	  {
+		  write_max (i,disp1ay[16][i-1]);
+	  }
+	  HAL_Delay (500);
+	  for(int i=1;i<9;i++)
+	  {
+		  write_max (i,disp1ay[24][i-1]);
+	  }
+	  HAL_Delay (500);
+	  for(int i=1;i<9;i++)
+	  {
+		  write_max (i,disp1ay[16][i-1]);
+	  }
+	  HAL_Delay (500);
+	  for(int i=1;i<9;i++)
+	  {
+		  write_max (i,disp1ay[24][i-1]);
+	  }
+	  HAL_Delay (500);
+	  for(int i=1;i<9;i++)
+	  {
+		  write_max (i,disp1ay[10][i-1]);
+	  }
+	  HAL_Delay (500);
+	  for(int i=1;i<9;i++)
+	  {
+		  write_max (i,disp1ay[28][i-1]);
+	  }
+	  HAL_Delay (500);
+	  for(int i=1;i<9;i++)
+	  {
+		  write_max (i,disp1ay[10][i-1]);
+	  }
+	  HAL_Delay (500);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -147,56 +240,37 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief SPI2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_SPI2_Init(void)
-{
-
-  /* USER CODE BEGIN SPI2_Init 0 */
-
-  /* USER CODE END SPI2_Init 0 */
-
-  /* USER CODE BEGIN SPI2_Init 1 */
-
-  /* USER CODE END SPI2_Init 1 */
-  /* SPI2 parameter configuration*/
-  hspi2.Instance = SPI2;
-  hspi2.Init.Mode = SPI_MODE_MASTER;
-  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi2.Init.DataSize = SPI_DATASIZE_4BIT;
-  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
-  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi2.Init.NSS = SPI_NSS_HARD_OUTPUT;
-  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
-  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
-  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
-  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  hspi2.Init.CRCPolynomial = 7;
-  hspi2.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
-  hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
-  if (HAL_SPI_Init(&hspi2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN SPI2_Init 2 */
-
-  /* USER CODE END SPI2_Init 2 */
-
-}
-
-/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12|GPIO_PIN_11, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA12 PA11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
